82be40fc011718ba62b3ba9ff754b0cb5ddc1203
Adroaldo <hygino@msn.com>
2024-09-08 19:01:28 -0300

    Adicionado freemarker

--- src/main/java/br/edu/utfpr/cp/esp/java/crudcidades/visao/CidadeController.java
+++ src/main/java/br/edu/utfpr/cp/esp/java/crudcidades/visao/CidadeController.java
@@ -1,13 +1,22 @@
 package br.edu.utfpr.cp.esp.java.crudcidades.visao;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CidadeController {
 
     @GetMapping("/")
-    public String listar() {
-        return "crud.html";
+    public String listar(Model memoria) {
+        var cidades = Set.of(
+                new Cidade("Coronel Vivida", "PR"),
+                new Cidade("Marcelino Ramos", "RS"),
+                new Cidade("São Domingos", "SC")
+        );
+        memoria.addAttribute("listaCidades", cidades);
+        return "/crud";
     }
 }
