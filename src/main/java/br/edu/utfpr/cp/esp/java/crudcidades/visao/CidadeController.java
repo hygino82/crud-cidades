d13f76d9b22811d05ef8950a86377eed1f81e4fe
Adroaldo <hygino@msn.com>
2024-09-22 19:13:39 -0300

    Implementação do CRUD

--- src/main/java/br/edu/utfpr/cp/esp/java/crudcidades/visao/CidadeController.java
+++ src/main/java/br/edu/utfpr/cp/esp/java/crudcidades/visao/CidadeController.java
@@ -1,22 +1,86 @@
 package br.edu.utfpr.cp.esp.java.crudcidades.visao;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CidadeController {
+    private Set<Cidade> cidades;
+
+    public CidadeController() {
+        cidades = new HashSet<>();
+    }
 
     @GetMapping("/")
     public String listar(Model memoria) {
-        var cidades = Set.of(
-                new Cidade("Coronel Vivida", "PR"),
-                new Cidade("Marcelino Ramos", "RS"),
-                new Cidade("São Domingos", "SC")
-        );
+
         memoria.addAttribute("listaCidades", cidades);
+
         return "/crud";
     }
+
+    @PostMapping("/criar")
+    public String criar(Cidade cidade) {
+
+        cidades.add(cidade);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/excluir")
+    public String excluir(
+            @RequestParam String nome,
+            @RequestParam String estado) {
+        cidades.removeIf(
+                cidadeAtual -> cidadeAtual.getNome().equals(nome) &&
+                        cidadeAtual.getEstado().equals(estado));
+
+        return "redirect:/";
+    }
+
+    @GetMapping("preparaAlterar")
+    public String preparaAlterar(
+            @RequestParam String nome,
+            @RequestParam String estado,
+            Model memoria) {
+
+        var cidadeAtual = cidades.stream()
+        .filter(
+                cidade -> cidade.getNome().equals(nome) &&
+                cidade.getEstado().equals(estado)
+                ).findAny();
+
+                if(cidadeAtual.isPresent()){
+                    memoria.addAttribute("cidadeAtual", cidadeAtual.get());
+                    memoria.addAttribute("listaCidades", cidades);
+                }
+
+        return "/crud";
+    }
+
+    @PostMapping("/alterar")
+    public String alterar(
+        @RequestParam String nomeAtual,
+        @RequestParam String estadoAtual,
+        Cidade cidade
+    ) {
+        
+        cidades.removeIf(
+                cidadeAtual -> 
+                            cidadeAtual.getNome().equals(nomeAtual) &&
+                            cidadeAtual.getEstado().equals(estadoAtual));
+
+        criar(cidade);
+        return "redirect:/";
+    }
+    
 }
